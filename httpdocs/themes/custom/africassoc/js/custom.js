cf3ab94969fcb24190b6e829efd619e197f4f294
Medard Rebero <medard.rebero@gmail.com>
2020-07-10 17:34:01 +0200

    W10

--- httpdocs/themes/custom/africassoc/js/custom.js
+++ httpdocs/themes/custom/africassoc/js/custom.js
@@ -9,6 +9,7 @@
   Drupal.behaviors.africassoc = {
     attach: function (context, settings) {
       $('input#edit-field-event-date-value').datepicker({
+        format: 'dd/mm/yyyy',
         autoclose: true,
         clearBtn: true,
         todayHighlight: true
