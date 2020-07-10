f28b257e610bbe35a5092e29c3590636d8bb10be
Medard Rebero <medard.rebero@gmail.com>
2020-07-10 18:51:37 +0200

    W10

--- httpdocs/themes/custom/africassoc/js/custom.js
+++ httpdocs/themes/custom/africassoc/js/custom.js
@@ -9,7 +9,7 @@
   Drupal.behaviors.africassoc = {
     attach: function (context, settings) {
       $('input#edit-field-event-date-value').datepicker({
-        format: 'dd/mm/yyyy',
+        format: 'dd/mm/yyyy', //Very important. This is used by "africassoc_utils_views_query_alter"
         autoclose: true,
         clearBtn: true,
         todayHighlight: true
